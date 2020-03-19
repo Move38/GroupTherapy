84f3ff73960b0b21267124c1b6eccafe0b54d4a9
ReluctantPirate <reluctantpirategames@gmail.com>
2020-03-19 15:03:06 -0400

    New balancing, but not perfect

--- GroupTherapy.ino
+++ GroupTherapy.ino
@@ -18,6 +18,7 @@ Timer phaseTimer;
 Timer flickerTimer;
 #define FLICKER_DURATION 75
 #define WAIT_PHASE_COLOR_CHANCE 500
+#define INTROVERT_CHANCE 40//out of 100 chance of an extrovert
 
 //Timer pulseTimer;
 //#define PULSE_DURATION 500
@@ -66,30 +67,42 @@ void loop() {
 void waitLoop() {
 
   //determine neighborhood
-  byte extrovertNeighbors = 0;
-  byte introvertNeighbors = 0;
-  FOREACH_FACE(f) {
-    if (!isValueReceivedOnFaceExpired(f)) {//neighbor!
-      byte neighborPersonality = getPersonality(getLastValueReceivedOnFace(f));
-      if (neighborPersonality == INTROVERT) {
-        introvertNeighbors++;
-      } else if (neighborPersonality = EXTROVERT) {
-        extrovertNeighbors++;
+  if (!isAlone()) { //only do all this stuff if you have neighbors at all
+    byte extrovertNeighbors = 0;
+    byte introvertNeighbors = 0;
+    byte neighborTotal = 0;
+    FOREACH_FACE(f) {
+      if (!isValueReceivedOnFaceExpired(f)) {//neighbor!
+        neighborTotal++;
+        byte neighborPersonality = getPersonality(getLastValueReceivedOnFace(f));
+        if (neighborPersonality == INTROVERT) {
+          introvertNeighbors++;
+        } else if (neighborPersonality = EXTROVERT) {
+          extrovertNeighbors++;
+        }
       }
     }
-  }
-  //  if (personality == INTROVERT) {
-  //    introvertNeighbors++;
-  //  } else if (personality == EXTROVERT) {
-  //    extrovertNeighbors++;
-  //  }
+    //remember to count yourself
+    neighborTotal++;
+    if (personality == INTROVERT) {
+      introvertNeighbors++;
+    } else if (personality == EXTROVERT) {
+      extrovertNeighbors++;
+    }
 
-  //change personality based on this information
-  if (introvertNeighbors > extrovertNeighbors) {
-    personality = EXTROVERT;
-  } else if (extrovertNeighbors > introvertNeighbors) {
-    personality = INTROVERT;
-  }
+    //change personality based on this information
+    //how many introverts should there be?
+    byte introvertsNeeded = (neighborTotal * INTROVERT_CHANCE) / 100;
+    if (introvertsNeeded == 0) {
+      introvertsNeeded = 1;
+    }
+
+    if (introvertNeighbors > introvertsNeeded) {
+      personality = EXTROVERT;
+    } else if (introvertNeighbors < introvertsNeeded) {
+      personality = INTROVERT;
+    }
+  }//end neighborhood check
 
   //listen for the next phase being triggered
   if (phaseTriggered(PLAY) || phaseTimer.isExpired()) {
@@ -120,7 +133,7 @@ void resultLoop() {
     phaseTimer.set(WAIT_PHASE_TIME);
     phase = WAIT;
     //set random personality
-    if (random(1) == 1) {
+    if (random(100) < INTROVERT_CHANCE) {
       personality = INTROVERT;
     } else {
       personality = EXTROVERT;
